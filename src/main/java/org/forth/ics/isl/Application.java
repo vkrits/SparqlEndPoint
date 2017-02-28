e5f89104b1d76b0a7d4799e0e7c1a09463da9ab7
Vangelis Kritsotakis <vangelis_kritsotakis@yahoo.com>
2017-02-28 16:31:18 +0200

    In comments war deployment

--- src/main/java/org/forth/ics/isl/Application.java
+++ src/main/java/org/forth/ics/isl/Application.java
@@ -2,6 +2,8 @@ package org.forth.ics.isl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * SparQL Endpoint client application for blazegraph using SpringMVC and AngulaJS
@@ -10,8 +12,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 
 @SpringBootApplication
+//public class Application extends SpringBootServletInitializer {
 public class Application {
-
+	
+	/*
+	 * Used only when deployed as war file on existing container
+	 */
+	/*
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+	*/
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
